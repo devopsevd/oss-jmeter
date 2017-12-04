8b3cf19eafb87528b9ac0f6c09b718726595262b
devopsdemoaws <devopsevd@gmail.com>
2017-12-04 08:41:14 +0000

    exec-jmeter.sh edited online with Bitbucket

--- exec-jmeter.sh
+++ exec-jmeter.sh
@@ -12,7 +12,7 @@ for filename in scripts/*.jmx; do
     NAME=$(basename $filename)
     NAME="${NAME%.*}"
     eval "docker cp $filename master:$WDIR/scripts/"
-    eval "docker exec -t master /bin/bash -c 'mkdir $NAME && cd $NAME && ../bin/jmeter -n -t ../$filename -R$SLAVE_IP'"
+    eval "docker exec -t master /bin/bash -c 'mkdir $NAME && cd $NAME && ../bin/jmeter -Jjmeter.save.saveservice.output_format=xml -Jjmeter.save.saveservice.response_data=true -Jjmeter.save.saveservice.samplerData=true -Jjmeter.save.saveservice.requestHeaders=true -Jjmeter.save.saveservice.url=true -Jjmeter.save.saveservice.responseHeaders=true -n -t ../$filename -R$SLAVE_IP'"
     eval "docker cp master:$WDIR/$NAME results/"
 done
 
