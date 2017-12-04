4bd00c5987dcfaa0f2f56dccd8499ad64c29868b
devopsdemoaws <devopsevd@gmail.com>
2017-12-04 07:52:08 +0000

    exec-jmeter.sh edited online with Bitbucket

--- exec-jmeter.sh
+++ exec-jmeter.sh
@@ -3,7 +3,7 @@
 COUNT=${1-1}
 
 docker build -t jmeter-base jmeter-base
-docker-compose build && docker-compose up -d && docker-compose scale master=1 slave=$COUNT
+docker-compose build && docker-compose up -d && docker-compose --scale master=1 slave=$COUNT
 
 SLAVE_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep slave | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
 WDIR=`docker exec -it master /bin/pwd | tr -d '\r'`
