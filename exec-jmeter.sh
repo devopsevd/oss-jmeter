7215c85fb67f541e4cfccf373eb5989cc4b44c86
devopsdemoaws <devopsevd@gmail.com>
2017-12-04 08:11:52 +0000

    exec-jmeter.sh edited online with Bitbucket

--- exec-jmeter.sh
+++ exec-jmeter.sh
@@ -6,13 +6,13 @@ docker build -t jmeter-base jmeter-base
 docker-compose build && docker-compose up -d && docker-compose scale master=1 slave=$COUNT
 
 SLAVE_IP=$(docker inspect -f '{{.Name}} {{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -aq) | grep slave | awk -F' ' '{print $2}' | tr '\n' ',' | sed 's/.$//')
-WDIR=`docker exec -it master /bin/pwd | tr -d '\r'`
+WDIR=`docker exec -t master /bin/pwd | tr -d '\r'`
 mkdir -p results
 for filename in scripts/*.jmx; do
     NAME=$(basename $filename)
     NAME="${NAME%.*}"
     eval "docker cp $filename master:$WDIR/scripts/"
-    eval "docker exec -it master /bin/bash -c 'mkdir $NAME && cd $NAME && ../bin/jmeter -n -t ../$filename -R$SLAVE_IP'"
+    eval "docker exec -t master /bin/bash -c 'mkdir $NAME && cd $NAME && ../bin/jmeter -n -t ../$filename -R$SLAVE_IP'"
     eval "docker cp master:$WDIR/$NAME results/"
 done
 
